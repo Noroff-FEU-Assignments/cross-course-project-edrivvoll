6c6a1a7debaea945ef47eb9566def9dae9aaf074
edrivvoll <espendr@outlook.com>
2023-10-19 23:46:50 +0200

    .js create container added

--- js/script.js
+++ js/script.js
@@ -2,13 +2,33 @@
 
 const url = "https://api.noroff.dev/api/v1/square-eyes"
 
+const carousellContainer = document.querySelector(".carousell-container")
+
+const movieCategories = ["top rated", "action", "comedy", "horror", "drama", "for kids"]
+
+function createCategories() {
+
+    for (i = 0; i < movieCategories.length; i++) {
+        /* console.log(movieCategories[i].replace(" ", "_")) */
+        carousellContainer.innerHTML += `
+                    <div class="carousel">
+                        <h2>${movieCategories[i]}</h2>
+                        <ul class="${movieCategories[i].replace(" ", "_")} carousel_ul" >
+                        </ul>
+                    </div>
+        `
+    }
+}
+
+createCategories();
+
+
 const resultsContainerTopRatad = document.querySelector(".top_rated");
 const resultsContainerAction = document.querySelector(".action");
 const resultsContainerComedy = document.querySelector(".comedy");
 const resultsContainerHorror = document.querySelector(".horror");
 const resultsContainerDrama = document.querySelector(".drama");
 const resultsContainerForKids = document.querySelector(".for_kids");
-const resultsContainerAllMovies = document.querySelector(".all_movies");
 
 async function makeApiCall(cont) {
 
@@ -18,16 +38,23 @@ async function makeApiCall(cont) {
 
     const facts = results;
 
+
+
     for (let i = 0; i < facts.length; i++) {
-        console.log(facts[i].title);
+        /* console.log(facts[i].title); */
+
+
+        /* if (facts[i].toLowerCase.gener === movieCategories[i].toLowerCase) {
+            console.log(facts[i].gener)
+        } */
         cont.innerHTML += `<li>
                                             <a href="products/product.html"
                                             ><img
                                                 src="${facts[i].image}"
-                                                alt="Hobbs and Shaw poster"
+                                                alt="${facts[i].title}"
                                                 class="carousel_img full_img" /><img
                                                 src="${facts[i].image}"
-                                                alt="Hobbs and Shaw poster"
+                                                alt="${facts[i].title}"
                                                 class="carousel_img cut_img"
                                             /></a>
                                         </li>`
@@ -35,10 +62,16 @@ async function makeApiCall(cont) {
 
 }
 
-makeApiCall(resultsContainerTopRatad);
-makeApiCall(resultsContainerAction);
-makeApiCall(resultsContainerComedy);
-makeApiCall(resultsContainerHorror);
-makeApiCall(resultsContainerDrama);
-makeApiCall(resultsContainerForKids);
-makeApiCall(resultsContainerAllMovies);
+function createCarousel() {
+    makeApiCall(resultsContainerTopRatad);
+    makeApiCall(resultsContainerAction);
+    makeApiCall(resultsContainerComedy);
+    makeApiCall(resultsContainerHorror);
+    makeApiCall(resultsContainerDrama);
+    makeApiCall(resultsContainerForKids);
+}
+
+// -------------------------------------------------
+
+
+createCarousel()
